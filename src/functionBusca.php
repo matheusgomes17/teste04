06aedd72f6d4d7cd9ef1dc263e2e0ca2b0f4e826
Matheus Gomes <zuza-cs@hotmail.com>
2015-01-06 19:05:17 -0200

    corrigido a função busca()

--- src/functionBusca.php
+++ src/functionBusca.php
@@ -15,7 +15,7 @@ function busca() {
     $pdo = conectarDb();
     $palavra = addslashes(trim($_POST['buscar']));
     try {
-        $query = $pdo->prepare("SELECT * FROM pages WHERE conteudo_titulo LIKE :busca");
+        $query = $pdo->prepare("SELECT * FROM pages WHERE conteudo_titulo LIKE :busca OR conteudo_principal LIKE :busca OR conteudo_content LIKE :busca");
         $query->bindValue(":busca", "%{$palavra}%");
         $query->execute();
     } catch (PDOException $e) {
@@ -25,7 +25,7 @@ function busca() {
         echo '<h3>A palavra <strong>' . $palavra . '</strong> retornou: ' . $query->rowCount() . ' resultados </h3>';
         $listar = $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
         foreach ($listar as $value):
-            echo "<h4><a href=\"{$value["pages"]}\">" . strip_tags($value[$palavra]) . "</a></h4>";
+            echo "<h4><a href=\"{$value["pages"]}\">" . strip_tags($value["conteudo_titulo"]) . "</a></h4>";
         endforeach;
     } else {
         echo 'Nenhum resultado encontrado com a palavra <strong>' . $palavra . '</strong>';
